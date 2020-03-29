e07e9ae5da14128e6e754b0d9da4a3d7c0cadd75
t1emp0 <rafa.beaus@gmail.com>
2020-03-29 19:05:57 +0200

    Improved carta: added navigation, refactored code

--- src/Carta.js
+++ src/Carta.js
@@ -2,100 +2,106 @@ import React from "react";
 
 import {
   makeStyles,
-  ButtonBase,
-  Container,
-  CardActionArea,
-  CardMedia,
-  CardContent
+  ButtonBase
 } from "@material-ui/core";
 
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { Link, Switch, Route, BrowserRouter } from "react-router-dom";
-
-import Italia from "./Italia";
+import { Link } from "react-router-dom";
 
 import pizza from "./assets/pizza.png";
+import japones from "./assets/japones.jpg";
 
-import japones from "./assets/japones.jpg"
 // LINK PER PODER ENTENDRE AIXÔ:
 // https://material-ui.com/components/buttons/
 
 const images = [
   {
     file: pizza,
-    title: "Italià"
+    title: "Italià",
+    subtitle: "Pizza, Pasta, Antipasti"
   },
   {
     file: japones,
-    title: "Japonés"
+    title: "Japonés",
+    subtitle: "Sushi, Noodles"
+  },
+  {
+    file: japones,
+    title: "Americà",
+    subtitle: "Burgers, Meats, Sides"
+  },
+  {
+    file: japones,
+    title: "Mexicà",
+    subtitle: "Burritos, Tacos, Acompañamientos"
+  },
+  {
+    file: japones,
+    title: "Begudes",
+    subtitle: "Burritos, Tacos, Acompañamientos"
+  },
+  {
+    file: japones,
+    title: "Postres",
+    subtitle: "Burritos, Tacos, Acompañamientos"
   }
 ];
 
 const buttonItemMenu = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
     minWidth: 300,
-    width: '100%',
+    width: "100%"
   },
   image: {
-    position: 'relative',
+    position: "relative",
     height: 200,
-    width: '90%',
-    marginTop: '40px',
-    marginBot: '20px',
-    '&:hover, &$focusVisible': {
+    width: "90%",
+    marginTop: "20px",
+    marginBot: "10px",
+    "&:hover, &$focusVisible": {
       zIndex: 1,
-      '& $imageBackdrop': {
-        opacity: 0.15,
-      },
-      '& $imageMarked': {
-        opacity: 0,
-      },
-      '& $imageTitle': {
-        border: '4px solid currentColor',
-      },
-    },
+      "& $imageBackdrop": {
+        opacity: 0.25
+      }
+    }
   },
   focusVisible: {},
   imageButton: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.common.white,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white
   },
   imageSrc: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%"
   },
   imageBackdrop: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
     backgroundColor: theme.palette.common.black,
-    opacity: 0.4,
-    transition: theme.transitions.create('opacity'),
+    opacity: 0.55,
+    transition: theme.transitions.create("opacity")
   },
   imageTitle: {
-    position: 'relative',
-    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`
   },
-  imageMarked: {
-    height: 3,
-    width: 18,
-    backgroundColor: theme.palette.common.white,
-    position: 'absolute',
-    bottom: -2,
-    left: 'calc(50% - 9px)',
-    transition: theme.transitions.create('opacity'),
+  imageSubtitle: {
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`
   },
 }));
 
@@ -103,19 +109,20 @@ export default function Carta() {
   const classes = buttonItemMenu();
 
   return (
-    
     <div align="center">
-      {images.map(image =>(
+      {images.map(image => (
         <ButtonBase
-        focusRipple
-        key={image.title}
-        className={classes.image}
-        focusVisibleClassName={classes.focusVisible}
-      >
+          focusRipple
+          key={image.title}
+          className={classes.image}
+          focusVisibleClassName={classes.focusVisible}
+          component={Link}
+          to="/fhc6/italia"
+        >
           <span
             className={classes.imageSrc}
             style={{
-              backgroundImage: `url(${image.file})`,
+              backgroundImage: `url(${image.file})`
             }}
           />
           <span className={classes.imageBackdrop} />
@@ -123,16 +130,20 @@ export default function Carta() {
             <Typography
               variant="h2"
               color="inherit"
-              className={classes.imageTitle}>
-
+              className={classes.imageTitle}
+            >
               {image.title}
-              <span className={classes.imageMarked} />
+              <Typography
+                variant="h6"
+                color="inherit"
+                className={classes.imageSubtitle}
+              >
+                {image.subtitle}
+              </Typography>
             </Typography>
           </span>
         </ButtonBase>
       ))}
-    </div>  
-      
-
+    </div>
   );
 }
